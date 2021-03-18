950f54ceefe52c69b3f3e0e3d3972e557dd7bf13
vradicevic <valentin.radicevic@gmail.com>
2021-03-19 00:14:13 +0100

    konvertanje frameova

--- BMA_v1/BMA_Enviroment/my_bma.c
+++ BMA_v1/BMA_Enviroment/my_bma.c
@@ -1,18 +1,18 @@
 #include "my_bma.h"
 
-int blockMatchingMYBMA(int16_t** vectors, uint8_t* currentFrame, uint8_t* prevFrame, int searchArea) {
+int blockMatchingMYBMA(int16_t** vectors, uint8_t* currentFrame, uint8_t* prevFrame, int step) {
     Point prevMacroblockCoo;
     int deviation, x, y;
     Point privFrom, privTo;
     int index = 0;
+
+
     for (y = 0; y < HEIGHT - (HEIGHT % BLOCK_SIZE); y += BLOCK_SIZE) {
         for (x = 0; x < WIDTH - ((WIDTH % BLOCK_SIZE)); x += BLOCK_SIZE) {
             prevMacroblockCoo.x = x;
             prevMacroblockCoo.y = y;
             prevMacroblockCoo = getCenter(prevMacroblockCoo);
             prevMacroblockCoo = getUpperLeft(prevMacroblockCoo);
-
-
             privFrom = prevMacroblockCoo;
 
 
@@ -23,7 +23,7 @@ int blockMatchingMYBMA(int16_t** vectors, uint8_t* currentFrame, uint8_t* prevFr
                 //float varin = ((float)deviation)/(BLOCK_SIZE*BLOCK_SIZE);
                 //Vps_printf("Variance deviation %f", varin);
 
-                privTo = getBestMatchMYBMA(prevMacroblockCoo, currentFrame, prevFrame, searchArea);
+                privTo = getBestMatchMYBMA(prevMacroblockCoo, currentFrame, prevFrame, step);
 
 
             }
@@ -40,56 +40,85 @@ int blockMatchingMYBMA(int16_t** vectors, uint8_t* currentFrame, uint8_t* prevFr
                 vectors[2][index] = (int16_t)privTo.x;
                 vectors[3][index] = (int16_t)privTo.y;
                 vectors[4][index] = (int16_t)length;
-                vectors[5][index] = (int16_t)CAST_ANGLE(calculateAngle2Points(privTo, privFrom));
+                vectors[5][index] = CAST_ANGLE(calculateAngle2Points(privTo, privFrom));
                 index++;
             }
             //printf("(%d,%d)", matches[index].x, matches[index].y);
 
 
+
+
         }
     }
+
     return index;
 
 }
 
-Point getBestMatchMYBMA(Point prevMacroblockCoo, uint8_t* currentFrame, uint8_t* prevFrame, int searchArea) {
+Point getBestMatchMYBMA(Point prevMacroblockCoo, uint8_t* currentFrame, uint8_t* prevFrame, int step) {
     Point best;
-    Point pointsSA[2];
-    getSearchArea(prevMacroblockCoo, searchArea, pointsSA);
+    Point points[9];
     Point currentMacroblockCoo;
-    int x, y;
-    float minSUMDIFF = 99999.999;
-    float SUMDIFF;
-    best = prevMacroblockCoo;
 
-    currentMacroblockCoo.x = prevMacroblockCoo.x;
-    currentMacroblockCoo.y = prevMacroblockCoo.y;
-    SUMDIFF = calculateSUMDIFF(currentFrame, prevFrame, currentMacroblockCoo, prevMacroblockCoo);
-    if (SUMDIFF < minSUMDIFF) {
-        minSUMDIFF = SUMDIFF;
-        best = currentMacroblockCoo;
-    }
+    /////blockSize*blockSize
+    //////postavljanje na sredinu makrobloka
+    points[0] = getCenter(prevMacroblockCoo);
 
-    for (y = pointsSA[0].y; y < pointsSA[1].y; y++) {
-        for (x = pointsSA[0].x; x < pointsSA[1].x; x++) {
-            currentMacroblockCoo.x = x;
-            currentMacroblockCoo.y = y;
 
-            SUMDIFF = calculateSUMDIFF(currentFrame, prevFrame, currentMacroblockCoo, prevMacroblockCoo);
-            
-            if (SUMDIFF < minSUMDIFF) {
-                minSUMDIFF = SUMDIFF;
-                best = currentMacroblockCoo;
+    float minMad = 99999.999;
+    float MAD;
+    best = points[0];
+    int i;
+
+    while (step >= 1) {
+
+        points[1].x = points[0].x;              //up
+        points[1].y = points[0].y - step;
+        points[2].x = points[0].x;              //down
+        points[2].y = points[0].y + step;
+        points[3].x = points[0].x - step;           //left
+        points[3].y = points[0].y;
+        points[4].x = points[0].x + step;           //right
+        points[4].y = points[0].y;
+        points[5].x = points[0].x - step;       //upperLeft
+        points[5].y = points[0].y - step;
+        points[6].x = points[0].x + step;       //upperRight
+        points[6].y = points[0].y - step;
+        points[7].x = points[0].x - step;       //lowerLeft
+        points[7].y = points[0].y + step;
+        points[8].x = points[0].x + step;       //lowerRight
+        points[8].y = points[0].y + step;
+        for (i = 0; i < 9; i++) {
+            currentMacroblockCoo = getUpperLeft(points[i]);
+
+            MAD = calculateMAD(currentFrame, prevFrame, currentMacroblockCoo, prevMacroblockCoo);
+
+            if (MAD < minMad) {
+                //printf("Iteration:%d \tMAD: %f\n", iteration, MAD);
+                minMad = MAD;
+                best = points[i];
+
             }
+
         }
+        //printf("Iteration:%d \tMAD: %f\n", iteration, minMad);
+
+        points[0] = best;
+        /*if (minMad > 1.0 && (best.x != lastBest.x && best.y != lastBest.y)) {
+            step = step;
+            iteration++;
+        }
+        else { step = step / 2; iteration = 0; }*/
+        step = step / 2;
+
 
     }
-    if (minSUMDIFF > 0.01000) {
+
+    //printf("MinMAD: %f\n", minMad);
+    ///////// vracanje na gornji lijevi ugao makrobloka
+    best = getUpperLeft(best);
+    if (minMad > 3.00) {
         best = prevMacroblockCoo;
     }
-    else {
-        printf("minimalni sumdiff %f\n", minSUMDIFF);
-    }
-    
     return best;
 }
--- BMA_v1/BMA_Enviroment/my_bma.h
+++ BMA_v1/BMA_Enviroment/my_bma.h
@@ -3,7 +3,7 @@
 
 #include "bma_utils.h"
 
-int blockMatchingMYBMA(int16_t** vectors, uint8_t* currentFrame, uint8_t* prevFrame, int searchArea);
-Point getBestMatchMYBMA(Point prevMacroblockCoo, uint8_t* currentFrame, uint8_t* prevFrame, int searchArea);
+int blockMatchingMYBMA(int16_t** vectors, uint8_t* currentFrame, uint8_t* prevFrame, int step);
+Point getBestMatchMYBMA(Point prevMacroblockCoo, uint8_t* currentFrame, uint8_t* prevFrame, int step);
 
 #endif
