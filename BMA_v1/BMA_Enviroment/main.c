f6bf1c12de6be67423bd8891dacf5c316560c818
vradicevic <valentin.radicevic@gmail.com>
2021-03-24 00:44:34 +0100

    commmit

--- BMA_v1/BMA_Enviroment/main.c
+++ BMA_v1/BMA_Enviroment/main.c
@@ -7,14 +7,15 @@
 #include <time.h>
 #include "vectors_filtering.h"
 #include "hibrid_bma.h"
+#include "paths.h"
 
 
 
 int main(void) {
 	char* videopath = "D:\\Videosekvence\\yuv\\moving_dashboardYUV444FPS30.yuv";//"F:\\odabrani_mirna_centerYUYV.yuv"
-	char* outputYUYVPath = "D:\\Videosekvence\\yuv\\odabrani_moving_dashboardYUYVFPS30.yuv";
-	char* testYUV444 = "D:\\Videosekvence\\yuv\\tester444.yuv";
-	char* vectorsPath = "D:\\vektori\\vectors0.bin";
+	
+	
+	
 	char* imagePath = "H:\\image.yuv";
 	int frames[] = { 39,67,69,75,79,87,93,95,135,137,149,151,153,155,183,185,187,189,191,203,209,219,301,317,321,329,345,347,351,427,429,431,437,463,689,691 };
 	int framesNum = 36;
@@ -42,11 +43,11 @@ int main(void) {
 	}
 	uint8_t* currframey=(uint8_t*)malloc(WIDTH*HEIGHT*2);
 	uint8_t* prevframey= (uint8_t*)malloc(WIDTH * HEIGHT * 2);
-	uint8_t* image = readFrameFrom422YUYVVideo(outputYUYVPath, WIDTH, HEIGHT, 10);
+	uint8_t* image = readFrameFrom422YUYVVideo(videoMovingDashboard30FPS , WIDTH, HEIGHT, 10);
 	getYComponent_YUV422_YUYV(prevframey, image, WIDTH, HEIGHT);
 	free(image);
 
-	image = readFrameFrom422YUYVVideo(outputYUYVPath, WIDTH, HEIGHT, 11);
+	image = readFrameFrom422YUYVVideo(videoMovingDashboard30FPS, WIDTH, HEIGHT, 11);
 	getYComponent_YUV422_YUYV(currframey, image, WIDTH, HEIGHT);
 	free(image);
 
---
+++ BMA_v1/BMA_Enviroment/paths.h
@@ -0,0 +1,17 @@
+#ifndef _PATHS_H_
+#define _PATHS_H_
+char* videoMovingDashboard30FPS = "F:\\Videosekvence\\yuv\\odabrani_moving_dashboardYUYVFPS30.yuv";
+char* videoMovingDashboard60FPS = "F:\\Videosekvence\\yuv\\odabrani_moving_dashboardYUYVFPS60.yuv";
+
+char* videoMirnaLeft30FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
+char* videoMirnaLeft60FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
+
+char* videoMirnaRight30FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
+char* videoMirnaRight60FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
+
+char* videoMirnaCenter30FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
+char* videoMirnaCenter60FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
+
+char* vectorsPath = "D:\\vektori\\vectors0.bin";
+
+#endif // !_PATHS_H
