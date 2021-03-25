29a5030ca9b570a942b6702401fe8368f6bdd5e1
vradicevic <valentin.radicevic@gmail.com>
2021-03-25 14:28:17 +0100

    Update paths.h

--- BMA_v1/BMA_Enviroment/paths.h
+++ BMA_v1/BMA_Enviroment/paths.h
@@ -1,17 +1,18 @@
 #ifndef _PATHS_H_
 #define _PATHS_H_
 char* videoMovingDashboard30FPS = "F:\\Videosekvence\\yuv\\odabrani_moving_dashboardYUYVFPS30.yuv";
-char* videoMovingDashboard60FPS = "F:\\Videosekvence\\yuv\\odabrani_moving_dashboardYUYVFPS60.yuv";
+char* videoMovingDashboard60FPS = "D:\\Videosekvence\\yuv\\odabrani_moving_dashboardYUYVFPS60.yuv";
 
-char* videoMirnaLeft30FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
-char* videoMirnaLeft60FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
+char* videoMirnaLeft30FPS = "D:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
+char* videoMirnaLeft60FPS = "D:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
 
-char* videoMirnaRight30FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
-char* videoMirnaRight60FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
+char* videoMirnaRight30FPS = "D:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
+char* videoMirnaRight60FPS = "D:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
 
-char* videoMirnaCenter30FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
-char* videoMirnaCenter60FPS = "F:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
+char* videoMirnaCenter30FPS = "D:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS30.yuv";
+char* videoMirnaCenter60FPS = "D:\\Videosekvence\\yuv\\odabrani_mirna_leftYUYVFPS60.yuv";
 
-char* vectorsPath = "H:\\vektori\\vectors0.bin";
+char* vectorsPath = "D:\\vektori\\vectors0.bin";
+char* belongsToPath = "D:\\vektori\\belongsTo.bin";
 
 #endif // !_PATHS_H
